81f1c370adb20ebdf516dc091a51bdefc4a95462
Danny Weinberg <FuegoFro@gmail.com>
2020-12-15 19:25:55 -0800

    2020 Day 12

--- src/util/point.rs
+++ src/util/point.rs
@@ -7,11 +7,47 @@ pub struct Point {
 }
 
 impl_op_ex!(+ |a: &Point, b: &Point| -> Point { Point { x: a.x + b.x, y: a.y + b.y }});
+impl_op_ex!(*|a: &Point, b: i32| -> Point {
+    Point {
+        x: a.x * b,
+        y: a.y * b,
+    }
+});
+impl_op_ex!(*|a: &Point, b: u32| -> Point {
+    Point {
+        x: a.x * b as i32,
+        y: a.y * b as i32,
+    }
+});
 impl_op!(+= |a: &mut Point, b: Point| { *a = &*a + b });
 impl_op!(+= |a: &mut Point, b: &Point| { *a = &*a + b });
 
 impl Point {
+    pub const UP: Point = Point { x: 0, y: 1 };
+    pub const DOWN: Point = Point { x: 0, y: -1 };
+    pub const LEFT: Point = Point { x: -1, y: 0 };
+    pub const RIGHT: Point = Point { x: 1, y: 0 };
+
+    pub fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+
     pub fn l1_dist(&self) -> i32 {
         self.x.abs() + self.y.abs()
+    }
+
+    pub fn rotate_about_origin_deg(&self, deg: u32) -> Point {
+        let deg_mod = ((deg % 360) + 360) % 360;
+        let (sin, cos) = match deg_mod {
+            0 => (0, 1),
+            90 => (1, 0),
+            180 => (0, -1),
+            270 => (-1, 0),
+            _ => panic!("Can only handle multiples of 90 degrees, got {}", deg),
+        };
+        Point {
+            x: self.x * cos - self.y * sin,
+            y: self.x * sin + self.y * cos,
+        }
     }
 }
