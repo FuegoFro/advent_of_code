d43abd56c98381ae9b62c21b04db550a5ce1f093
Danny Weinberg <FuegoFro@gmail.com>
2021-12-01 22:52:24 -0800

    2021 day 2

---
+++ template_day/mod.rs
@@ -0,0 +1,7 @@
+pub fn main() {
+    let input = include_str!("example_input.txt").trim();
+    // let input = include_str!("actual_input.txt").trim();
+
+    println!("Part 1: {}", "Answer here");
+    // println!("Part 2: {}", "Answer here");
+}
