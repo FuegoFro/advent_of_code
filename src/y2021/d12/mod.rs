aeb4eefa3ee77cc659638206e8505959e2060df3
Danny Weinberg <FuegoFro@gmail.com>
2021-12-12 10:29:46 -0800

    Use references, small cleanups for 2021 day 12

--- src/y2021/d12/mod.rs
+++ src/y2021/d12/mod.rs
@@ -9,50 +9,47 @@ fn is_small_cave(s: &str) -> bool {
 }
 
 /// For now assume no two big caves are next to each other
-/// TODO - Can we make this fully use references?
-fn dfs_all_paths_recursive(
-    graph: &HashMap<&str, Vec<&str>>,
-    path_so_far: &mut Vec<String>,
-    visited_small_caves: &mut HashSet<String>,
-    all_paths: &mut Vec<Vec<String>>,
+fn dfs_all_paths_recursive<'container, 'node: 'container>(
+    graph: &HashMap<&'node str, Vec<&'node str>>,
+    path_so_far: &'container mut Vec<&'node str>,
+    visited_small_caves: &'container mut HashSet<&'node str>,
+    num_paths: &mut usize,
     has_revisited_small_cave: bool,
 ) {
-    let current_node = path_so_far
-        .last()
-        .map_or(START, |node| node.as_str())
-        .to_string();
+    let current_node = path_so_far.last().map_or(START, |node| node);
     if current_node == END {
         // Done with this path!
-        all_paths.push(path_so_far.clone());
+        *num_paths += 1;
         return;
     } else if current_node == START && !path_so_far.is_empty() {
         // Don't revisit the start
         return;
     }
 
-    // TODO - Can this state (insert then remove) be better encoded in the type system? Can this be a `match`?
     let has_visited_this_small_cave = visited_small_caves.contains(&current_node);
-    let is_revisiting_small_cave = if has_visited_this_small_cave {
-        if has_revisited_small_cave {
-            return;
-        } else {
-            true
-        }
-    } else {
+    let is_revisiting_small_cave = if !has_visited_this_small_cave {
+        // Not visited this yet, can't be revisiting it.
         false
+    } else if has_revisited_small_cave {
+        // Already revisited, bail.
+        return;
+    } else {
+        // This is our first time revisiting a small cave.
+        true
     };
 
     // Visit all reachable
+    // TODO - Can this state (insert then remove) be better encoded in the type system?
     if is_small_cave(&current_node) && !is_revisiting_small_cave {
-        visited_small_caves.insert(current_node.to_string());
+        visited_small_caves.insert(current_node);
     }
-    for reachable in graph[current_node.as_str()].iter() {
-        path_so_far.push(reachable.to_string());
+    for reachable in graph[current_node].iter() {
+        path_so_far.push(reachable);
         dfs_all_paths_recursive(
             graph,
             path_so_far,
             visited_small_caves,
-            all_paths,
+            num_paths,
             has_revisited_small_cave || is_revisiting_small_cave,
         );
         path_so_far.pop();
@@ -62,39 +59,33 @@ fn dfs_all_paths_recursive(
     }
 }
 
-fn dfs_all_paths(
-    graph: &HashMap<&str, Vec<&str>>,
-    all_two_small_cave_visits: bool,
-) -> Vec<Vec<String>> {
-    let mut all_paths = Vec::new();
+fn dfs_all_paths(graph: &HashMap<&str, Vec<&str>>, allow_two_small_cave_visits: bool) -> usize {
+    let mut num_paths = 0;
 
     dfs_all_paths_recursive(
         graph,
         &mut Vec::new(),
         &mut HashSet::new(),
-        &mut all_paths,
-        !all_two_small_cave_visits,
+        &mut num_paths,
+        !allow_two_small_cave_visits,
     );
-    all_paths
+    num_paths
 }
 
 pub fn main() {
     // let input = include_str!("example_input.txt").trim().replace("\r", "");
     let input = include_str!("actual_input.txt").trim().replace("\r", "");
 
-    // TODO - Can this be cleaner?
-    let forward = input
+    let graph = input
         .split("\n")
         .map(|line| line.split_once("-").unwrap())
-        .collect_vec();
-    let backward = forward.iter().map(|(a, b)| (*b, *a)).collect_vec();
-    let graph = forward
-        .into_iter()
-        .chain(backward.into_iter())
+        // Expand with both forwards and backwards links
+        // TODO - Turn this to just `.into_iter()` once using 2021 Edition.
+        .flat_map(|(a, b)| IntoIterator::into_iter([(a, b), (b, a)]))
         .into_group_map();
 
-    let all_paths = dfs_all_paths(&graph, false);
-    println!("Part 1: {}", all_paths.len());
-    let all_paths = dfs_all_paths(&graph, true);
-    println!("Part 2: {}", all_paths.len());
+    let num_paths = dfs_all_paths(&graph, false);
+    println!("Part 1: {}", num_paths);
+    let num_paths = dfs_all_paths(&graph, true);
+    println!("Part 2: {}", num_paths);
 }
